d63b8284ddce538d473192224b92c4220d477d7f
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-09-22 20:20:17 +0300

    -i флаг, изменение вывода

--- git_rg.py
+++ git_rg.py
@@ -10,7 +10,7 @@ from functools import partial
 from pathlib import Path
 from signal import SIG_DFL, SIGPIPE, signal
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "s3rgeym"
 
 # https://stackoverflow.com/questions/14207708/ioerror-errno-32-broken-pipe-when-piping-prog-py-othercmd
@@ -58,29 +58,26 @@ def is_compressed(f: typing.BinaryIO) -> bool:
         # https://stackoverflow.com/a/17176881/2240578
         return f.read(2) in [b"\x1f\x8b", b"\x78\x9c", b"\x78\x01", b"\x78\xda"]
     finally:
-        f.seek(f.tell() - 2)
+        f.seek(0)
 
 
 def git_readlines(file_path: Path) -> typing.Iterable[str]:
-    try:
-        with file_path.open("rb") as f:
-            if is_compressed(f):
-                decompressor = zlib.decompressobj()
-                buffer = ""
-                while chunk := f.read(1 << 16):
-                    decompressed = decompressor.decompress(chunk)
-                    buffer += decompressed.decode("utf-8", errors="ignore")
-                    *lines, buffer = buffer.split("\n")
-                    yield from map(str.rstrip, lines)
-                if buffer:
-                    yield buffer
-            else:
-                yield from map(
-                    str.rstrip,
-                    file_path.open("r", encoding="utf-8", errors="ignore"),
-                )
-    except Exception as e:
-        print_err(f"Error reading {file_path}: {e}")
+    with file_path.open("rb") as f:
+        if is_compressed(f):
+            decompressor = zlib.decompressobj()
+            buffer = ""
+            while chunk := f.read(1 << 16):
+                decompressed = decompressor.decompress(chunk)
+                buffer += decompressed.decode("utf-8", errors="ignore")
+                *lines, buffer = buffer.split("\n")
+                yield from map(str.rstrip, lines)
+            if buffer:
+                yield buffer
+        else:
+            yield from map(
+                str.rstrip,
+                map(partial(bytes.decode, errors="ignore"), f),
+            )
 
 
 def highlight_matches(text: str, matches: list[re.Match]) -> str:
@@ -124,7 +121,7 @@ def git_grep(
                 before_lines.append((linenum, line))
                 continue
 
-            print(f"{GREEN}{UNDERLINE}File: {file_path}{RESET}")
+            print(f"{GREEN}{UNDERLINE}Found: {file_path}{RESET}")
 
             while before_lines:
                 before_linenum, before_line = before_lines.popleft()
@@ -147,7 +144,7 @@ def git_grep(
                 except StopIteration:
                     break
     except Exception as e:
-        print_err(f"Error reading {file_path}: {e}")
+        print_err(f"{RED}Error: {e}{RESET}")
 
 
 def git_rg(
@@ -180,6 +177,13 @@ def main(argv: typing.Sequence[str] | None = None) -> None:
         help="Directory path to search (default: current directory)",
     )
     parser.add_argument(
+        "-i",
+        "--ignore",
+        help="Case insensetive search",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
         "-B",
         "-b",
         "--before",
@@ -207,7 +211,7 @@ def main(argv: typing.Sequence[str] | None = None) -> None:
 
     with suppress(KeyboardInterrupt):
         git_rg(
-            re.compile(args.pattern),
+            re.compile(args.pattern, re.IGNORECASE if args.ignore else 0),
             args.path,
             args.before,
             args.after,
